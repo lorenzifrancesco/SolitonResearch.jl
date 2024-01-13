2c29bbc2a57ec5c2d53d142398b9a8409cbfd6e9
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-13 17:40:01 +0100

    change: shift of starting condition from prepare_for_collision to imprint_vel_set_barr

--- src/utils/sim_utils.jl
+++ src/utils/sim_utils.jl
@@ -66,33 +66,20 @@ function prepare_for_collision!(sd, gamma; use_precomputed_gs=false, info=false)
     # write the initial state into sim
     if length(sim.N) == 1
       @unpack_Sim sim
-      if sim.equation == NPSE_plus
-        x0 = L[1] / 8
-      else
-        x0 = L[1] / 4
-      end
-      shift = Int(x0 / L[1] * N[1])
       iswitch = 1
       x = X[1]
       psi_0 = uu
-      xspace!(psi_0, sim)
-      psi_0 .= circshift(psi_0, shift)
-      kspace!(psi_0, sim)
       @assert isapprox(nsk(psi_0, sim), 1.0, atol=1e-9)
       # time_steps = Int(ceil((tf - ti) / dt))
       @pack_Sim! sim
     else
       @unpack_Sim sim
       x0 = L[1] / 4
-      shift = Int(x0 / L[1] * N[1])
       iswitch = 1
       x = X[1] |> real
       y = X[2] |> real
       z = X[3] |> real
       psi_0 = CuArray(uu)
-      xspace!(psi_0, sim)
-      psi_0 = circshift(CuArray(psi_0), (shift, 0, 0))
-      kspace!(psi_0, sim)
       @assert isapprox(nsk(psi_0, sim), 1.0, atol=1e-9)
       time_steps = Int(ceil((tf - ti) / dt))
       @pack_Sim! sim
@@ -102,78 +89,9 @@ function prepare_for_collision!(sd, gamma; use_precomputed_gs=false, info=false)
   return sd
 end
 
-function imprint_vel_set_bar(
-  sim::Sim{1,Array{Complex{Float64}}};
-  vv::Float64=0.0,
-  bb::Float64=0.0,
-  bw::Float64=0.5,
-  dt_set::Float64=0.001,
-  time_step_limit::Int64=5000,
-)
-  throw("Deepcopy is deprecated")
-  simc = deepcopy(sim)
-  @unpack_Sim simc
-  x = X[1] |> real
-  @. V0 = bb * exp(-(x / bw)^2 / 2) # central barrier
-  if sim.equation == NPSE_plus
-    x0 = L[1] / 8
-  else
-    x0 = L[1] / 4
-  end
-  if vv == 0.0
-    tf = 2.0
-  else
-    tf = 2 * x0 / vv
-  end
-  t = LinRange(ti, tf, Nt)
-  time_steps = Int(floor((tf - ti) / dt_set))
-  if time_steps > time_step_limit
-    @warn "time_steps > $time_step_limit, clipping dt"
-    time_steps = time_step_limit
-    dt = (tf - ti) / time_steps
-  end
-  xspace!(psi_0, simc)
-  @. psi_0 = abs(psi_0) * exp(-im * (x) * vv)
-  kspace!(psi_0, simc)
-  @pack_Sim! simc
-  return simc
-end
-
-function imprint_vel_set_bar(
-  sim::Sim{3,CuArray{Complex{Float64}}};
-  vv::Float64=0.0,
-  bb::Float64=0.0,
-  bw::Float64=0.5,
-  dt_set::Float64=0.01, # TODO optimize
-  time_step_limit::Int64=5000,
-)
-
-  simc = deepcopy(sim)
-  @unpack_Sim simc
-  x = X[1] |> real
-  y = X[2] |> real
-  z = X[3] |> real
-  V0 = [1 / 2 * (z^2 + y^2) + bb * exp(-(x / bw)^2 / 2) for x in x, y in y, z in z] # central barrier
-  x0 = L[1] / 4
-  if vv == 0.0
-    tf = 2.0
-  else
-    tf = 2 * x0 / vv
-  end
-  t = LinRange(ti, tf, Nt)
-  time_steps = Int(floor((tf - ti) / dt_set))
-  if time_steps > time_step_limit
-    @warn "time_steps > $time_step_limit, clipping dt"
-    time_steps = time_step_limit
-    dt = (tf - ti) / time_steps
-  end
-  xspace!(psi_0, simc)
-  @. psi_0 = abs(psi_0) * exp(-im * (x) * vv)
-  kspace!(psi_0, simc)
-  @pack_Sim! simc
-  return simc
-end
-
+```
+Inplace function 1D
+```
 function imprint_vel_set_bar!(
   sim::Sim{1,Array{Complex{Float64}}};
   vv::Float64=0.0,
@@ -187,6 +105,12 @@ function imprint_vel_set_bar!(
   x = X[1] |> real
   @. V0 = bb * exp(-(x / bw)^2 / 2) # central barrier
   x0 = L[1] / 4
+  if sim.equation == NPSE_plus
+    x0 = L[1] / 8
+  else
+    x0 = L[1] / 4
+  end
+  shift = Int(x0 / L[1] * N[1])
   if vv == 0.0
     tf = 2.0
   else
@@ -201,12 +125,59 @@ function imprint_vel_set_bar!(
     print("\n Setting dt to $dt")
   end
   xspace!(psi_0, sim)
+  psi_0 .= circshift(psi_0, shift)
   @. psi_0 = abs(psi_0) * exp(-im * (x) * vv)
   kspace!(psi_0, sim)
   @pack_Sim! sim
   return sim
 end
 
+```
+Copy function 1D
+```
+function imprint_vel_set_bar(
+  sim::Sim{1,Array{Complex{Float64}}};
+  vv::Float64=0.0,
+  bb::Float64=0.0,
+  bw::Float64=0.5,
+  dt_set::Float64=0.001,
+  time_step_limit::Int64=5000,
+)
+  @warn "Deepcopy is deprecated"
+  simc = deepcopy(sim)
+  @unpack_Sim simc
+  x = X[1] |> real
+  @. V0 = bb * exp(-(x / bw)^2 / 2) # central barrier
+  if sim.equation == NPSE_plus
+    x0 = L[1] / 8
+  else
+    x0 = L[1] / 4
+  end
+  shift = Int(x0 / L[1] * N[1])
+  if vv == 0.0
+    tf = 2.0
+  else
+    tf = 2 * x0 / vv
+  end
+  t = LinRange(ti, tf, Nt)
+  time_steps = Int(floor((tf - ti) / dt_set))
+  if time_steps > time_step_limit
+    @warn "time_steps > $time_step_limit, clipping dt"
+    time_steps = time_step_limit
+    dt = (tf - ti) / time_steps
+  end
+  xspace!(psi_0, simc)
+  psi_0 .= circshift(psi_0, shift)
+  @. psi_0 = abs(psi_0) * exp(-im * (x) * vv)
+  kspace!(psi_0, simc)
+  @pack_Sim! simc
+  return simc
+end
+
+
+```
+Inplace function 3D
+```
 function imprint_vel_set_bar!(
   sim::Sim{3,CuArray{Complex{Float64}}};
   vv::Float64=0.0,
@@ -227,6 +198,7 @@ function imprint_vel_set_bar!(
   else
     tf = 2 * x0 / vv
   end
+  shift = Int(x0 / L[1] * N[1])
   t = LinRange(ti, tf, Nt)
   time_steps = Int(floor((tf - ti) / dt_set))
   if time_steps > time_step_limit
@@ -235,10 +207,51 @@ function imprint_vel_set_bar!(
     dt = (tf - ti) / time_steps
   end
   xspace!(psi_0, sim)
+  psi_0 .= circshift(CuArray(psi_0), (shift, 0, 0))
   @. psi_0 = abs(psi_0) * exp(-im * (x) * vv)
   kspace!(psi_0, sim)
   @pack_Sim! sim
   return sim
+end
+
+```
+Copy function 3D
+```
+function imprint_vel_set_bar(
+  sim::Sim{3,CuArray{Complex{Float64}}};
+  vv::Float64=0.0,
+  bb::Float64=0.0,
+  bw::Float64=0.5,
+  dt_set::Float64=0.01, # TODO optimize
+  time_step_limit::Int64=5000,
+)
+  @warn "Deepcopy is deprecated!"
+  simc = deepcopy(sim)
+  @unpack_Sim simc
+  x = X[1] |> real
+  y = X[2] |> real
+  z = X[3] |> real
+  V0 = [1 / 2 * (z^2 + y^2) + bb * exp(-(x / bw)^2 / 2) for x in x, y in y, z in z] # central barrier
+  x0 = L[1] / 4
+  if vv == 0.0
+    tf = 2.0
+  else
+    tf = 2 * x0 / vv
+  end
+  shift = Int(x0 / L[1] * N[1])
+  t = LinRange(ti, tf, Nt)
+  time_steps = Int(floor((tf - ti) / dt_set))
+  if time_steps > time_step_limit
+    @warn "time_steps > $time_step_limit, clipping dt"
+    time_steps = time_step_limit
+    dt = (tf - ti) / time_steps
+  end
+  xspace!(psi_0, simc)
+  psi_0 .= circshift(CuArray(psi_0), (shift, 0, 0))
+  @. psi_0 = abs(psi_0) * exp(-im * (x) * vv)
+  kspace!(psi_0, simc)
+  @pack_Sim! simc
+  return simc
 end
 
 function set_g!(sim::Sim{1,Array{Complex{Float64}}}, gamma_param::Float64=0.4)
