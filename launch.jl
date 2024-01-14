0e70ddf14e783c5d9a807e56d398e5ada1850ca5
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-14 16:24:24 +0100

    change: launcher reads from a configuration CSV file

--- launch.jl
+++ launch.jl
@@ -1,5 +1,7 @@
-using Pkg
-Pkg.activate(".")
-
+using CSV, DataFrames, Tables, Printf
 using SolitonResearch
-tiles(number_of_tiles=50, equation="Np", messages=true)
+
+conf = CSV.read("conf.csv", DataFrame)
+@INFO conf
+@info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
+tiles(number_of_tiles=10, equation="Np", messages=true)
