fb7e7ed6864561a8c0b76e0b064ccc82b67b8aaf
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-18 20:20:49 +0100

    sim: evaluate NPSE_plus maximum to set the collapse_threshold

--- launch.jl
+++ launch.jl
@@ -4,4 +4,4 @@ using SolitonResearch
 conf = CSV.read("conf.csv", DataFrame)
 @info conf
 @info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
-fill_tiles(number_of_tiles=50, eqs=[NPSE_plus])
+fill_tiles(number_of_tiles=10, eqs=[NPSE_plus], return_maximum=true)
