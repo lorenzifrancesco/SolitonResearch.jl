c4253c5547329f0c31ffffd102a6f8f6d09ec51d
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-18 20:41:53 +0100

    change: launch routine (is incomplete)

--- launch.jl
+++ launch.jl
@@ -1,7 +1,10 @@
 using CSV, DataFrames, Tables, Printf
 using SolitonResearch
 
-conf = CSV.read("conf.csv", DataFrame)
-@info conf
-@info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
-fill_tiles(number_of_tiles=10, eqs=[NPSE_plus], return_maximum=true)
+# conf = CSV.read("conf.csv", DataFrame)
+# @info conf
+# @info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
+N = 10
+return_maximum = true
+@info @sprintf("N=%i, return_maximum=%d", N, return_maximum)
+fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
