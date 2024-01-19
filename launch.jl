1229cf3a81ef019279d7130551a162dbac942b70
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-19 18:05:16 +0100

    fix: merge issues

--- launch.jl
+++ launch.jl
@@ -1,12 +1,6 @@
 using CSV, DataFrames, Tables, Printf
 using SolitonResearch
 
-<<<<<<< HEAD
-conf = CSV.read("conf.csv", DataFrame)
-@info conf
-@info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
-fill_tiles(number_of_tiles=50, eqs=[NPSE_plus], return_maximum=true)
-=======
 print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ BEGIN OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@\n")
 cmd = `date`
 print(read(cmd, String))
@@ -21,4 +15,3 @@ print(get_current_commit_data())
 print("=================================================================\n")
 fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
 print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ END OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
->>>>>>> 56ab84b491822295de54da76f93cc6b60d67f63b
