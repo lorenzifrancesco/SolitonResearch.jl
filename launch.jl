4d8e03bebe7daed406138ddea7b0a7b225884f5a
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-19 12:47:38 +0100

    add: file selection and commit data gathering

--- launch.jl
+++ launch.jl
@@ -1,10 +1,15 @@
 using CSV, DataFrames, Tables, Printf
 using SolitonResearch
 
+print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ BEGIN OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@\n")
 # conf = CSV.read("conf.csv", DataFrame)
 # @info conf
 # @info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
-N = 10
+N = 20
 return_maximum = true
-@info @sprintf("N=%i, return_maximum=%d", N, return_maximum)
-fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
+@printf("N=%i, return_maximum=%d", N, return_maximum)
+print("\n=================================================================\n")
+print(get_current_commit_data())
+print("=================================================================\n")
+# fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
+print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ END OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
--- src/utils/data_utils.jl
+++ src/utils/data_utils.jl
@@ -50,7 +50,7 @@ function humanize()
 end
 
 function human_readable_soliton(
-  save_path="results/", 
+  save_path="results/",
   human_folder="human_readable/",
   file_name="soliton_")
   if isfile(save_path * "gs_dict.jld2")
@@ -75,7 +75,7 @@ function human_readable_soliton(
 end
 
 function human_readable_tile(
-  save_path="results/", 
+  save_path="results/",
   human_folder="human_readable/",
   file_name="tiles___")
   if isfile(save_path * "tile_dict.jld2")
@@ -94,26 +94,26 @@ function human_readable_tile(
 end
 
 function plot_tiles(matrix=nothing)
-    # pyplot(size=(300, 220))
-    # pyplot(size=(300, 260))
-    # backend(:pyplot)
-    tile_file = "results/tile_dict.jld2"
-    @assert isfile(tile_file)
-    td = JLD2.load(tile_file)
-    delete!(td, hs("CQ", 0.65))
-    vaxis = nothing
-    baxis = nothing
-    kk = []
-    for (k, v) in td
-        push!(kk, k)
-        @info "found" ihs(k)
-        if ihs(k)[1] == "G3" || ihs(k)[1] == "Np"
-            preprocess_tiles_3d!(v)
-        end
-        (vaxis, baxis) = get_pavement_axes(v)
-        plot_pavement(vaxis, baxis, v, k)
+  # pyplot(size=(300, 220))
+  # pyplot(size=(300, 260))
+  # backend(:pyplot)
+  tile_file = "results/tile_dict.jld2"
+  @assert isfile(tile_file)
+  td = JLD2.load(tile_file)
+  delete!(td, hs("CQ", 0.65))
+  vaxis = nothing
+  baxis = nothing
+  kk = []
+  for (k, v) in td
+    push!(kk, k)
+    @info "found" ihs(k)
+    if ihs(k)[1] == "G3" || ihs(k)[1] == "Np"
+      preprocess_tiles_3d!(v)
     end
-    nothing
+    (vaxis, baxis) = get_pavement_axes(v)
+    plot_pavement(vaxis, baxis, v, k)
+  end
+  nothing
 end
 
 function get_pavement_axes(v)
@@ -123,43 +123,43 @@ function get_pavement_axes(v)
 end
 
 function plot_pavement(
-  vaxis::LinRange{Float64, Int64}, 
-  baxis::LinRange{Float64, Int64}, 
-  v::Matrix{Float64}, 
-  k::String; 
+  vaxis::LinRange{Float64,Int64},
+  baxis::LinRange{Float64,Int64},
+  v::Matrix{Float64},
+  k::String;
   title=L"T")
   ht2 = heatmap(
     vaxis,
     baxis,
     v,
-    clabels = true,
-    xlabel = L"v",
-    ylabel = L"b",
-    colorbar_title = title,
+    clabels=true,
+    xlabel=L"v",
+    ylabel=L"b",
+    colorbar_title=title,
     # legend=false,
-    aspect_ratio = :equal,
-    top_margin = 0 * Plots.mm,
-    bottom_margin = 0 * Plots.mm,
-    left_margin = 0 * Plots.mm,
-    right_margin = 0 * Plots.mm,
+    aspect_ratio=:equal,
+    top_margin=0 * Plots.mm,
+    bottom_margin=0 * Plots.mm,
+    left_margin=0 * Plots.mm,
+    right_margin=0 * Plots.mm,
   )
-  plot!(ht2, vaxis, vaxis .^2)
+  plot!(ht2, vaxis, vaxis .^ 2)
   savefig(ht2, "media/tiles_" * string(ihs(k)) * "_ht.pdf")
   # v, mask = process_tiles(v)
   mask = v
-  ht = contour(vaxis, baxis, v, clabels = true, xlabel = L"v", ylabel = L"b")
+  ht = contour(vaxis, baxis, v, clabels=true, xlabel=L"v", ylabel=L"b")
   contour!(
-      ht,
-      vaxis,
-      baxis,
-      mask,
-      levels = [0.0],
-      color = :turbo,
-      linestyle = :dot,
-      linewidth = 1.8,
+    ht,
+    vaxis,
+    baxis,
+    mask,
+    levels=[0.0],
+    color=:turbo,
+    linestyle=:dot,
+    linewidth=1.8,
   )
   alpha = 1.0
-  plot!(ht, vaxis, vaxis.^2 * alpha)
+  plot!(ht, vaxis, vaxis .^ 2 * alpha)
   if k == "0000"
     savefig(ht, "media/tiles_test_ct.pdf")
   else
@@ -169,63 +169,101 @@ function plot_pavement(
 end
 
 function preprocess_tiles_3d!(tt)
-    for bar = 1:size(tt)[1]
-        for vel = 2:size(tt)[2]
-            if tt[vel, bar] - tt[vel-1, bar] > 0.02
-                tt[vel, bar] = NaN
-                for velx = vel:size(tt)[2]
-                    tt[velx, bar] = NaN
-                end
-            end
+  for bar = 1:size(tt)[1]
+    for vel = 2:size(tt)[2]
+      if tt[vel, bar] - tt[vel-1, bar] > 0.02
+        tt[vel, bar] = NaN
+        for velx = vel:size(tt)[2]
+          tt[velx, bar] = NaN
         end
+      end
     end
-    return tt
+  end
+  return tt
 end
 
 function process_tiles(tt)
-    mask = ones(size(tt))
-    # FIXME the names
-    for bar = 1:size(tt)[1]
-        for vel = 2:size(tt)[2]
-            if abs(tt[vel, bar] - tt[vel-1, bar]) > 0.3
-                tt[vel, bar] = NaN
-                for velx = 1:vel
-                    mask[velx, bar] = 0.0
-                end
-            end
+  mask = ones(size(tt))
+  # FIXME the names
+  for bar = 1:size(tt)[1]
+    for vel = 2:size(tt)[2]
+      if abs(tt[vel, bar] - tt[vel-1, bar]) > 0.3
+        tt[vel, bar] = NaN
+        for velx = 1:vel
+          mask[velx, bar] = 0.0
         end
+      end
     end
+  end
 
-    flag_prev = true
+  flag_prev = true
+  flag_curr = true
+  posv = 1
+  posb = 1
+  for vel = 1:size(tt)[2]
     flag_curr = true
-    posv = 1
-    posb = 1
-    for vel = 1:size(tt)[2]
-        flag_curr = true
-        for bar = 1:size(tt)[1]
-            if mask[vel, bar] == 0.0
-                flag_curr = false
-                posv = vel
-                posb = bar
-            end
-        end
-        if flag_prev == false && flag_curr == true
-            for velx = 1:posv
-                mask[velx, posb] = NaN
-            end
-            return tt, mask
-        end
-        flag_prev = flag_curr
+    for bar = 1:size(tt)[1]
+      if mask[vel, bar] == 0.0
+        flag_curr = false
+        posv = vel
+        posb = bar
+      end
     end
-    return tt, mask
+    if flag_prev == false && flag_curr == true
+      for velx = 1:posv
+        mask[velx, posb] = NaN
+      end
+      return tt, mask
+    end
+    flag_prev = flag_curr
+  end
+  return tt, mask
 end
 
-function csv2color(file_name;
+function csv2color(file_name=nothing;
   path="results/")
   pyplot()
-  vals = Tables.matrix(CSV.read(path*file_name*".csv", DataFrame))
+  if isnothing(file_name)
+    file_name = choose_file(path)
+  end
+  vals = Tables.matrix(CSV.read(path * file_name * ".csv", DataFrame))
   (vx, bx) = get_pavement_axes(vals)
   p = heatmap(vx, bx, vals, title=file_name, interpolate=false)
-  savefig(p, "media/colorized_csv"*file_name*".pdf")
+  savefig(p, "media/colorized_csv" * file_name * ".pdf")
   nothing
+end
+
+"""
+  Choose a file between the ones in a given path.
+  Check if the selected number is valid, and if the item is a file
+"""
+function choose_file(path)
+  @printf("Pick a file in <%15s>", path)
+  files = readdir(path)
+  for (index, file) in enumerate(files)
+    println("[$index] $file")
+  end
+  try
+    user_input = parse(Int, readline())
+    chosen_file = files[user_input]
+    println("You chose: $chosen_file")
+    @assert isfile(chosen_file)
+    return chosen_file
+  catch err
+    println("Invalid input. Please enter a valid index.")
+    throw(err)
+  end
+end
+
+"""
+  Calling git with a shell command, 
+  find the commit name and id of the project in pwd
+"""
+function get_current_commit_data(repo_path=".")
+  cmd_cd = `cd $repo_path`
+  # run(cmd_cd)
+  cmd_git = `git log -1`
+  delimiter = "\ngitgitgitgitgitgitgitgitgitgitgitgitgitgitgitgit\n"
+  log_string = read(cmd_git, String)
+  return log_string
 end
