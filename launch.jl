960989e0ec4c8005f84d8a14e3114bba83ad65f7
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-19 13:09:36 +0100

    change: print date in launch.jl

--- launch.jl
+++ launch.jl
@@ -2,14 +2,16 @@ using CSV, DataFrames, Tables, Printf
 using SolitonResearch
 
 print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ BEGIN OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@\n")
-# conf = CSV.read("conf.csv", DataFrame)
-# @info conf
-# @info @sprintf("LAUNCHING %ix%i tiles, equation = %s", conf.N[1], conf.N[1], conf.eq[1])
-N = 20
+cmd = `date`
+print(read(cmd, String))
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+# TODO load configuration from input
+N = 10
 return_maximum = true
 @printf("N=%i, return_maximum=%d", N, return_maximum)
 print("\n=================================================================\n")
 print(get_current_commit_data())
 print("=================================================================\n")
-# fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
+fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
 print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ END OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
--- launch.sh
+++ launch.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-date | tee -a stdout.log
-date >> stderr.log
 julia --threads=12 launch.jl > >(tee -a stdout.log) 2> >(tee -a stderr.log >&2)
 
