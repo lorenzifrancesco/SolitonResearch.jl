5cf617c4f51f1aa66ca366884fd9fa016bfb00b7
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-01-19 19:12:50 +0100

    change: launch message

--- launch.jl
+++ launch.jl
@@ -7,11 +7,16 @@ print(read(cmd, String))
 print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
 # TODO load configuration from input
-N = 50
-return_maximum = true
-@printf("N=%i, return_maximum=%d", N, return_maximum)
+
 print("\n=================================================================\n")
 print(get_current_commit_data())
 print("=================================================================\n")
+N = 50
+return_maximum = true
+@printf("==============      N=%i, return_maximum=%d       ==============\n", N, return_maximum)
 fill_tiles(number_of_tiles=N, eqs=[NPSE_plus], return_maximum=return_maximum)
+
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+cmd = `date`
+print(read(cmd, String))
 print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@ END OF RUN @@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
