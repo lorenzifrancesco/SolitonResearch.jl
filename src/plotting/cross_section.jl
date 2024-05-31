b154e0bc7516ec0aee3aa9ddd8d76d280a623a48
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-05-31 11:11:00 +0200

    add: breather configuration

--- src/plotting/cross_section.jl
+++ src/plotting/cross_section.jl
@@ -1,7 +1,9 @@
 using SolitonDynamics, SolitonResearch, CUDA
 CUDA.allowscalar()
 sim = load_simulation("input/", GPE_3D)
-prepare_for_collision(sim, 0.67*3; use_precomputed=false)
+prepare_for_collision!(sim, 0.65; use_precomputed_gs=false)
+sim.g /=3
 sol = runsim(sim)
+
 u = sol[1].u
 transverse_animation(u, sim.Nt, sim)
